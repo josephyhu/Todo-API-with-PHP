46113ad1b921de0507f0e944808687176c5a2adc
Joseph Yhu <xbhlx7@gmail.com>
2019-11-08 16:54:22 -0500

    Update

--- src/dependencies.php
+++ src/dependencies.php
@@ -30,6 +30,7 @@ $container['db'] = function ($c) {
     $pdo = new PDO($db['dsn'].':'.$db['database']);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $pdo->exec( 'PRAGMA foreign_keys = ON;' );
     return $pdo;
 };
 
